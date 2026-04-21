c2152e0bb0bfe1ef66641cd3b2dfe40b8b47b1b3
neveshardd <118855875+neveshardd@users.noreply.github.com>
2026-04-21 20:13:01 -0300

    fix: images

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "crafatar.com",
       },
+      {
+        protocol: "https",
+        hostname: "67a661c7cc7210d66352eaad9da96403.r2.cloudflarestorage.com",
+      },
     ],
   },
 };
